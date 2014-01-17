3304d1ea490a4772aa2746da3257990ccf5acce8
Joseph DiVita <joediv31@gmail.com>
2014-01-17 12:11:31 -0600

    adding connection handler + travis updates
    adding connection handler + travis updates
    
    adding connection handler + travis updates
    
    adding connection handler + travis updates
    trying test redis server on different port
    
    fixing travis config
    
    adding redis connection handler
    
    require redis
    
    updating redis test env
    
    updating gemfile lock
    
    require active support core ext
    
    updated spec helper
    
    updated connection spec
    
    simplifying connection

---
+++ spec/connection_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe Redistat::Connection do
+  before do
+    # Reset any connections
+    Redistat::Connection.redis = nil
+    Redistat::Connection.namespace = nil
+  end
+
+  describe '#establish_connection' do
+    before do
+      redis = Redis.new(host: 'foo', port: 9000)
+      Redistat::Connection.establish_connection(redis, 'bar')
+    end
+
+    it 'properly sets the redis connection' do
+      expect(Redistat::Connection.redis.client.host).to eq('foo')
+      expect(Redistat::Connection.redis.client.port).to eq(9000)
+    end
+
+    it 'properly sets the namespace' do
+      expect(Redistat::Connection.namespace).to eq('bar')
+    end
+  end
+
+  context 'when setting options one at a time' do
+    before do
+      Redistat::Connection.redis = Redis.new(host: 'foo', port: 1111)
+      Redistat::Connection.namespace = 'bar'
+    end
+
+    it 'properly sets the redis connection' do
+      expect(Redistat::Connection.redis.client.host).to eq('foo')
+      expect(Redistat::Connection.redis.client.port).to eq(1111)
+    end
+
+    it 'properly sets the namespace' do
+      expect(Redistat::Connection.namespace).to eq('bar')
+    end
+  end
+end
